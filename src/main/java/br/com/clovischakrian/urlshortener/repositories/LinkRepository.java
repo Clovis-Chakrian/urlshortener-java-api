80d097b488ca424684d2365014ea2e402dd623fe
Clóvis Chakrian <clovischakriandev@gmail.com>
2024-09-21 17:06:29 -0300

    feat: added getAll method at repository

--- src/main/java/br/com/clovischakrian/urlshortener/repositories/LinkRepository.java
+++ src/main/java/br/com/clovischakrian/urlshortener/repositories/LinkRepository.java
@@ -2,9 +2,14 @@ package br.com.clovischakrian.urlshortener.repositories;
 
 import br.com.clovischakrian.urlshortener.entities.Link;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LinkRepository extends JpaRepository<Link, Integer> {
     public Optional<Link> getByLinkId(int linkId);
+
+    @Query("select * from links")
+    public List<Link> getAll();
 }
