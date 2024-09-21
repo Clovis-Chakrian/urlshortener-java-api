bf30c74529b96f52181754bb794db37dc4414ddb
Clóvis Chakrian <clovischakriandev@gmail.com>
2024-09-21 18:51:17 -0300

    feat: created method to redirect

--- src/main/java/br/com/clovischakrian/urlshortener/controllers/LinkController.java
+++ src/main/java/br/com/clovischakrian/urlshortener/controllers/LinkController.java
@@ -3,12 +3,15 @@ package br.com.clovischakrian.urlshortener.controllers;
 import br.com.clovischakrian.urlshortener.dtos.LinkDto;
 import br.com.clovischakrian.urlshortener.entities.Link;
 import br.com.clovischakrian.urlshortener.services.ILinkService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,13 +27,23 @@ public class LinkController {
         return new ResponseEntity<>(links, HttpStatus.OK);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Link> redirectById(@PathVariable int id, HttpServletResponse httpResponse) throws IOException {
+        Optional<Link> link = linkService.getLinkById(id);
+
+        if (link.isEmpty()) ResponseEntity.notFound();
+
+        httpResponse.sendRedirect(link.get().getRedirectLink());
+        return null;
+    }
+
     @PostMapping
     public ResponseEntity<Link> insertLink(@Valid @RequestBody LinkDto linkDto) {
         Link link = linkService.insertLink(linkDto);
         return new ResponseEntity<>(link, HttpStatus.CREATED);
     }
 
-    @PutMapping
+    @PutMapping("/{id}")
     public ResponseEntity<Link> updateLink(@PathVariable int id, @Valid @RequestBody LinkDto linkDto) {
         Optional<Link> link = linkService.updateLink(linkDto, id);
 
