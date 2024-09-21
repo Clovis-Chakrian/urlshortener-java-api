48e0de6045b5309c16c1b6e80ed977046b9871c5
Clóvis Chakrian <clovischakriandev@gmail.com>
2024-09-21 19:32:40 -0300

    fix: consertando sequencial como id

--- src/main/java/br/com/clovischakrian/urlshortener/entities/Link.java
+++ src/main/java/br/com/clovischakrian/urlshortener/entities/Link.java
@@ -14,7 +14,7 @@ import java.util.Date;
 @EqualsAndHashCode(of = "linkId")
 public class Link {
     @Id()
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int linkId;
     private String redirectLink;
     private Date createdAt;
